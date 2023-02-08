aada6b69df2b6fa579fe06309659ccc55268237a
dxt99 <ericlouis19x@gmail.com>
2023-02-08 12:17:23 +0700

    feat: defined canvas dimensions in px

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,10 @@
+// changes screen x coordinates to gl float coordinates
 function to_float_x(pos, size){
     mid = size / 2
     return pos / mid - 1
 }
 
+// changes screen y coordinates to gl float coordinates
 function to_float_y(pos, size){
     mid = size / 2
     return 1 - pos / mid
